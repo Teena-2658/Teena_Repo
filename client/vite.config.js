5b34287f6ba633605e6744a51b64db98c6d1fb1c
Teena Devray <teenadevray24@gmail.com>
2025-08-31 14:02:50 +0530

    final1

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Teena_Portfolio/'  // must match your GitHub repo name
+  base: './' // ✅ For Vercel
 })
